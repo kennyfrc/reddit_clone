2e72a5784aad0164618d0f497b716bfa0f420d4a
kennyfrc <fxkennyfrc@gmail.com>
2015-11-05 00:40:11 +0800

    redone everything. just the backend

---
+++ db/seeds.rb
@@ -0,0 +1,27 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+50.times do 
+  Post.create!(
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph
+  )
+end
+  posts = Post.all
+
+100.times do
+  Comment.create!(
+    post: posts.sample,
+    body: Faker::Lorem.paragraph
+  )
+end
+
+puts "Seeding done"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
