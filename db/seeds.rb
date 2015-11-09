eff431284ab3dfd5cf6ff01f5ddae0399e05aa2e
kennyfrc <fxkennyfrc@gmail.com>
2015-11-10 00:44:39 +0800

    associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,21 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
+5.times do
+    user = User.new(
+        name: Faker::Name.name,
+        email: Faker::Internet.email,
+        password: Faker::Lorem.characters(10)
+      )
+    user.skip_confirmation!
+    user.save!
+end
+users = User.all
+
+
 50.times do 
   Post.create!(
+    user: users.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
   )
@@ -17,10 +30,19 @@ end
 
 100.times do
   Comment.create!(
+    # user: users.sample,
     post: posts.sample,
     body: Faker::Lorem.paragraph
   )
 end
+
+user = User.first
+user.skip_confirmation!
+user.update_attributes!(
+  email: "fxkennyfrc@gmail.com",
+  password: "absinthe12"
+)
+
 
 puts "Seeding done"
 puts "#{Post.count} posts created"
