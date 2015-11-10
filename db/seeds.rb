26c757a7eaa63383576bc7f44b900bc212ee0ef0
kennyfrc <fxkennyfrc@gmail.com>
2015-11-11 00:47:15 +0800

    added topics

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,10 +18,19 @@
 end
 users = User.all
 
+15.times do
+  Topic.create!(
+      name: Faker::Lorem.sentence,
+      description: Faker::Lorem.paragraph
+    )
+end
+topics = Topic.all
+
 
 50.times do 
   Post.create!(
     user: users.sample,
+    topic: topics.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
   )
