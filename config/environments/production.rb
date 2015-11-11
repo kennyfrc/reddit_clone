df74bd3493ca1eca5a782ada6b65d8035e23b850
kennyfrc <fxkennyfrc@gmail.com>
2015-11-11 22:34:43 +0800

    configured app for production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = {host: 'lit-everglades-4091.herokuapp.com' }
+
+  config.force_ssl = true
+
 end
