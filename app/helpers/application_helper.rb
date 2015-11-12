9d430ead8362fce675bc3765993d76d0f6348a51
kennyfrc <fxkennyfrc@gmail.com>
2015-11-12 13:13:02 +0800

    post validations with new application_helper method that returns div tags. its a helper just to keep the views clean its a software design principle

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+=begin
+  the form_group_tag method returns a content_tag (which renders <div> </div>) so it should be used to wrap a small-ish form group  
+=end
+
+   def form_group_tag(errors, &block)
+     if errors.any?
+       content_tag :div, capture(&block), class: 'form-group has-error'
+     else
+       content_tag :div, capture(&block), class: 'form-group'
+     end
+   end
 end
