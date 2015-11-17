ad5f818185ab511f64d9e86a67fe64d60bdf0c2e
kennyfrc <fxkennyfrc@gmail.com>
2015-11-17 18:30:45 +0800

    just added some comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,17 @@
 class CommentsController < ApplicationController
 
   def create
-    @topic = Topic.find(params[:topic_id])
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.new(comment_params)
-    @comment.user = current_user
-    @comment.post = @post
+    @topic = Topic.find(params[:topic_id]) # to establish topic/X in topic/X/post/X/comments
+    @post = Post.find(params[:post_id]) #to establish post/X in topic/X/post/X/comments
+    @comment = @post.comments.new(comment_params) #to create a new comment (which is currently just part of post)
+    @comment.user = current_user 
+    @comment.post = @post #there's no topic because comment technically is not part of topic
     if @comment.save
       flash[:notice] = "Comment was saved"
-      redirect_to [@topic, @post]
+      redirect_to [@topic, @post] #just topic/X/post/X
     else
       flash[:error] = "Something went wrong. Please try again"
-      redirect_to [@topic, @post]
+      redirect_to [@topic, @post] #just topic/X/post/X
     end
   end
 
