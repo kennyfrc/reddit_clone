5f3abee97616cd50904a130397de1ea7c86134c7
kennyfrc <fxkennyfrc@gmail.com>
2015-12-05 17:12:57 +0800

    some big fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,15 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap
+
+var blocmetrics = {}; // we want to namespace this because we don't want it to "collide" with other code
+  blocmetrics.report = function(eventName){
+    var event = { name: eventName}; // you had an event object earlier. assign eventname to the name attribute
+    var request = new XMLHttpRequest(); // instantiate a new XMLHttpRequest. This is
+                                  // an API that provides client funcitonality for transferring data
+                                  // between a client and a server
+    request.open("POST", "http://localhost:8080/api/events", true); // sends a POST request to a specific url. true is for performign the task asynchronously
+    request.setRequestHeader('Content-Type', 'application/json'); // sets the value of a request header as part of a SET OF RULES (protocol) in the HTTP framework for sending and receving HTTP
+    request.send(JSON.stringify(event)); // send the request (stringified)
+  };
+
